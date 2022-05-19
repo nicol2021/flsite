675ed03288d1297e14ceebc1b47a4fadd0a9ed6a
nicol2021 <wolna2011@gmail.com>
2022-05-19 23:55:36 +0300

    fl_15_16

--- flsite.py
+++ flsite.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, render_template, request, g, flash, abort, redirect, url_for, session
 from FDataBase import FDataBase
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from UserLogin import UserLogin
 
 # конфигурация
 DATABASE = '/tmp/flsite.db'
@@ -13,8 +15,17 @@ SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
+login_manager.login_message_category = "success"
+
+@login_manager.user_loader
+def load_user(user_id):
+   print("load_user")
+   return UserLogin().fromDB(user_id, dbase)
 
 def connect_db():
    conn = sqlite3.connect(app.config['DATABASE'])
@@ -69,14 +80,40 @@ def addPost():
    return render_template('add_post.html', menu = dbase.getMenu(), title="Добавление статьи")
 
 @app.route("/post/<alias>")
+@login_required
 def showPost(alias):
    title, post = dbase.getPost(alias)
    if not title:
       abort(404)
    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
-@app.route("/login")
+# @app.route("/post/<alias>")
+# def showPost(alias):
+#    title, post = dbase.getPost(alias)
+#    if not title:
+#       abort(404)
+#    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+# @app.route("/login")
+# def login():
+#    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
+   if current_user.is_authenticated:
+      return redirect(url_for('profile'))
+   
+   if request.method == "POST":
+      user = dbase.getUserByEmail(request.form['email'])
+      if user and check_password_hash(user['psw'], request.form['psw']):
+         userlogin = UserLogin().create(user)
+         rm = True if request.form.get('remainme') else False
+         # login_user(userlogin)
+         login_user(userlogin, remember=rm)
+         # return redirect(url_for('index'))
+         # return redirect(url_for('profile'))
+         return redirect(request.args.get("next") or url_for("profile"))
+      flash("Неверная пара логин/пароль", "error")
    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
 
 # @app.route("/register")
@@ -99,6 +136,19 @@ def register():
       else:
          flash("Неверно заполнены поля", "error")
    return render_template("register.html", menu=dbase.getMenu(), title="Регистрация")
+
+@app.route('/logout')
+@login_required
+def logout():
+   logout_user()
+   flash("Вы вышли из аккаунта", "success")
+   return redirect(url_for('login'))
+
+@app.route('/profile')
+@login_required
+def profile():
+   return f"""<a href="{url_for('logout')}">Выйти из профиля</a>
+               user info: {current_user.get_id()}"""
 
 if __name__ == '__main__':
    # app.run(threaded=True, port=5000)
